71a4cb38e38dee464831c3ebdc279c25a1c140d7
hkeidenb <hasani.eidenberger@gmail.com>
2024-02-18 22:38:42 -0800

    JavaScript Variables

--- Assets/script.js
+++ Assets/script.js
@@ -1,8 +1,18 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-var today = moment()
+
+
 $(function () {
+  // Save the button
+  var SaveButton = $(.saveBtn);
+  //  Selectors for hours 9-5
+  var hours= [$(#hour-9), $(#hour-10), $(hour-11), $(hour-12), $(hour-13), $(hour-14), $(hour-15), $(hour-16), $(hour-17)]
+  // Saved data from local storage
+  var saved;
+
+// Using utiliizing JS 
+  var currentHour = dayjs().hour();
+  var hourIndex = currentHour - 9;
+  var today = dayjs();
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -21,4 +31,6 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+
 });
+  // use the id in the containing time-block as a key to save the user input in
